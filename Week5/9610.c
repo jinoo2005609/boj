f35f71b59726a8145746facd551a677ee0a9a6c9
Esherloon <jinoo2005609@gmail.com>
2024-05-16 18:24:16 +0900

    9610 Comment

--- Week5/9610.c
+++ Week5/9610.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-int q1=0, q2=0, q3=0, q4=0, axis=0;
+int q1=0, q2=0, q3=0, q4=0, axis=0; // 정의
 
-int di(int x, int y) {
-    if (x > 0 && y > 0) q1++;
-    else if (x < 0 && y > 0) q2++;
-    else if (x < 0 && y < 0) q3++;
-    else if (x > 0 && y < 0) q4++;
-    else axis++;
+int di(int x, int y) { // di 함수 정의
+    if (x > 0 && y > 0) q1++; // 1사분면
+    else if (x < 0 && y > 0) q2++; // 2사분면
+    else if (x < 0 && y < 0) q3++; // 3사분면
+    else if (x > 0 && y < 0) q4++; // 4사분면
+    else axis++; // 축
     return 0;
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    int n; // 정의
+    scanf("%d", &n); // 값 입력
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { // 입력받은 n만큼 반복
         int x, y;
-        scanf("%d %d", &x, &y);
-        di(x, y);
+        scanf("%d %d", &x, &y); // x, y 좌표값 입력
+        di(x, y); // di 함수 호출
     }
     printf("Q1: %d\nQ2: %d\nQ3: %d\nQ4: %d\nAXIS: %d\n", q1, q2, q3, q4, axis);
     return 0;
